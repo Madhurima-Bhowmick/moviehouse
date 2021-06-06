4fe678f61c7dc149d665a9022c5eaacd2ac91760
Madhurima <76059423+Madhurima-Bhowmick@users.noreply.github.com>
2021-06-07 04:16:24 +0530

    Add files via upload

---
+++ build/precache-manifest.efe90e692fc9b0153ef9db68c6d23511.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07b5885fad1310ae2c0a24c57085a824",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e789f01d29e0b577621",
+    "url": "/static/css/main.8e91e24f.chunk.css"
+  },
+  {
+    "revision": "4b192a9489e13fd5e8e8",
+    "url": "/static/js/2.738fa40d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.738fa40d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e789f01d29e0b577621",
+    "url": "/static/js/main.b97deec8.chunk.js"
+  },
+  {
+    "revision": "e23f96d817477a4cb65a",
+    "url": "/static/js/runtime-main.268a3519.js"
+  }
+]);
